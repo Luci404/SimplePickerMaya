595d2cce7db52782da0380883241d52d6f724a25
Storm Søndergaard <stormsoendergaard@gmail.com>
2022-01-04 23:43:37 +0100

    Colors and UI scale

--- SimplePickerMaya.py
+++ SimplePickerMaya.py
@@ -4,32 +4,23 @@ import typing
 
 class SelectorButton(QtWidgets.QPushButton):
     _objectName: str = ""
+    _UIScale: float = 1.0
 
-    def __init__(self, objectName: str, buttonLabel: str, posX: int, posY: int, sizeX: int, sizeY: int, parent: QtWidgets.QWidget):
+    def __init__(self, objectName: str, buttonLabel: str, buttonColor: str, posX: int, posY: int, sizeX: int, sizeY: int, parent: QtWidgets.QWidget):
         super().__init__(buttonLabel, parent)
-
         self._objectName = objectName
-                
+        self.setStyleSheet(f"background-color: {buttonColor}")
         self.setGeometry(posX, posY, sizeX, sizeY)
         self.clicked.connect(self.onButtonClicked)
-
         self.show()
 
     def onButtonClicked(self):
         if (cmds.objExists(self._objectName)):
             modifiers = QtWidgets.QApplication.keyboardModifiers()
-            if modifiers == (QtCore.Qt.ControlModifier | QtCore.Qt.ShiftModifier):
-                # Ctrl + Shift + Click
-                cmds.select(self._objectName, add=True)
-            elif modifiers == QtCore.Qt.ShiftModifier:
-                # Shift + Click
-                cmds.select(self._objectName, toggle=True)
-            elif modifiers == QtCore.Qt.ControlModifier:
-                # Ctrl + Click
-                cmds.select(self._objectName, deselect=True)
-            else:
-                # Click
-                cmds.select(self._objectName, replace=True)
+            if modifiers == (QtCore.Qt.ControlModifier | QtCore.Qt.ShiftModifier): cmds.select(self._objectName, add=True)
+            elif modifiers == QtCore.Qt.ShiftModifier: cmds.select(self._objectName, toggle=True)
+            elif modifiers == QtCore.Qt.ControlModifier: cmds.select(self._objectName, deselect=True)
+            else: cmds.select(self._objectName, replace=True)
         else:
             print(f"Error: Object '{self._objectName}', dose not exist!")
 
@@ -38,17 +29,19 @@ class SimpleMayaPicker(QtWidgets.QWidget):
     
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle("SimpleMayaPicker 1.0")
         self.setGeometry(300, 50, 766, 980)
-
         self.show()
+
+    def SetUIScale(self, scale: float):
+        self._UIScale = scale
         
-    def AddSelector(self, objectName: str, buttonLabel: str, posX: int, posY: int, sizeX: int, sizeY: int):
-        newSelectorButton = SelectorButton(objectName, buttonLabel, posX, posY, sizeX, sizeY, self)
+    def AddSelector(self, objectName: str, buttonLabel: str, buttonColor: str, posX: int, posY: int, sizeX: int, sizeY: int):
+        newSelectorButton = SelectorButton(objectName, buttonLabel, buttonColor, posX*self._UIScale, posY*self._UIScale, sizeX*self._UIScale, sizeY*self._UIScale, self)
         newSelectorButton.show()
         self._buttons.append(newSelectorButton)
 
 myPicker = SimpleMayaPicker()
-myPicker.AddSelector("L_Wrist_CTRL", "lWrist", 100, 10, 50, 30)
-myPicker.AddSelector("R_Wrist_CTRL", "rWrist", 100, 40, 50, 30)
+myPicker.SetUIScale(2)
+myPicker.AddSelector("L_Wrist_CTRL", "lWrist", "red", 100, 10, 50, 30)
+myPicker.AddSelector("R_Wrist_CTRL", "rWrist", "blue", 100, 50, 50, 30)
